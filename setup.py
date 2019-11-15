624c102c4f577bb212d2c37e6cde6ab0781be074
Kanmii <maneptha@gmail.com>
2019-11-15 12:18:37 +0100

    Removed pytest plugin from server package.

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ from setuptools import setup
 setup(
     name="resumemakr",
     packages=setuptools.find_packages(),
-    entry_points={"pytest11": ["name_of_plugin = server.pytest_django_run_first"]},
+    entry_points={
+        "pytest11": [
+            "name_of_plugin = resumemakr_pytest_plugins.pytest_django_run_first"
+        ]
+    },
 )
